095a056ffeb1f44a9f4130ea2b65fe928ccf23d8
Damian Frizzi <damian.f@v-designs.ch>
2015-01-06 20:17:07 +0100

    partial commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,34 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    watch: {
-      scripts: {
-        files: ['js/*.js', 'scss/*.scss'],
-        tasks: ['requirejs', 'sass'],
-        options: {
-          spawn: false,
-          reload: true
+        requirejs: {
+            compile: {
+                options: {
+                    name: 'app',
+                    baseUrl: 'js/',
+                    mainConfigFile: 'js/app.js',
+                    out: 'js/built.js',
+                    preserveLicenseComments: false,
+                    findNestedDependencies: true,
+                    removeCombined: true
+                }
+            }
         },
-      }
-    },
 
-    requirejs: {
-      compile: {
-        options: {
-          name: 'app',
-          baseUrl: 'js/',
-          mainConfigFile: 'js/app.js',
-          out: 'js/built.js',
-          preserveLicenseComments: false
+        cssmin: {
+            target: {
+                files: {
+                    'css/main.min.css': ['css/main.css']
+                }
+            }
         }
-      }
-    },
+    });
 
-    sass: {
-      dist: {
-        options: {
-          style: 'compressed'
-        },
-        files: {
-          'css/main.css': 'scss/main.scss'
-        }
-      }
-    }
-  });
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  // Load plugins
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-
-  // What happens when we "grunt" the terminal
-  grunt.registerTask('default', ['requirejs', 'sass']);
-
+    // What happens when we "grunt" the terminal
+    grunt.registerTask('default', ['requirejs', 'cssmin']);
 };
